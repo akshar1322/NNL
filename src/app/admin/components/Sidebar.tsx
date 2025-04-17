b1323bd3da04fff46963b7d0155215c266aba9e8
Akshar Patel528@gmail.com <aksharpatel528@gmailcom>
2025-04-17 14:51:31 +0530

    fix: remove user management page and related API routes

--- src/app/admin/components/Sidebar.tsx
+++ src/app/admin/components/Sidebar.tsx
@@ -13,8 +13,6 @@ export default function Sidebar({ username, role }: { username: string; role: st
         </div>
         <nav className="p-6 space-y-4">
           <Link href="/admin/dashboard" className="block hover:text-lime-400">Home</Link>
-          
-          <Link href="/admin/users" className="block hover:text-lime-400">Users</Link>
           <Link href="/admin/export" className="block hover:text-lime-400">Export</Link>
         </nav>
         <LogoutButton/>
